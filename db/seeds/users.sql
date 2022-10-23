90250c812cdeabc22e428b3f70182fe3df2f2ddf
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-23 18:15:25 +0000

    Adjusted seeds to take in first name and last name instead of just name for users

--- db/seeds/users.sql
+++ db/seeds/users.sql
@@ -1,7 +1,7 @@
-INSERT INTO users (name, email, handler, password) VALUES ('Jordan', 'jordan2515@gmail.com', '@mr_guerrero', '12345');
+INSERT INTO users (first_name, last_name, email, handler, password) VALUES ('Jordan', 'Martinez', 'jordan2515@gmail.com', '@mr_guerrero', '12345');
 
-INSERT INTO users (name, email, handler, password) VALUES ('Alex', 'alex0616@gmail.com', '@alex0616', '12345');
+INSERT INTO users (first_name, last_name, email, handler, password) VALUES ('Alex', 'Jeong', 'alex0616@gmail.com', '@alex0616', '12345');
 
-INSERT INTO users (name, email, handler, password) VALUES ('Santiago', 'santi250@gmail.com', '@santi250', '12345');
+INSERT INTO users (first_name, last_name, email, handler, password) VALUES ('Santiago', 'Curbelo', 'santi250@gmail.com', '@santi250', '12345');
 
-INSERT INTO users (name, email, handler, password) VALUES ('Veronica', 'ver56@gmail.com', '@ver250', '12345');
+INSERT INTO users (first_name, last_name, email, handler, password) VALUES ('Veronica', 'Martins', 'ver56@gmail.com', '@ver250', '12345');
