ba74dba715fd30e03291e5b60fe21561b1a1f021
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-23 16:18:18 +0000

    Refactor so the search route considers query strings

--- routes/search.js
+++ routes/search.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  if (req.query.q) {
+    res.send('query exists, so I will put posts that match the query value');
+  }
   res.redirect('/');
 });
 
