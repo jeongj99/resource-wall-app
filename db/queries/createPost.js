e20565197bf43c227ba5dbd1234ec8fad9484a86
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-24 22:05:57 +0000

    Added a create post function and hooked it up to our routes/post file, adjusted create.ejs so that form names would match names used in our function

--- db/queries/createPost.js
+++ db/queries/createPost.js
@@ -1,16 +1,40 @@
 const db = require('../connection');
 
-const createPost = (createQuery) => {
-  let queryString =
-  'INSERT INTO created_posts (title, description, url, photo_url) VALUES ($1, $2, $3, $4);'
-  return pool.query(queryString, [createQuery.title, createQuery.description, createQuery.url, createQuery.photo_url])
+const createPost = (createObj) => {
+  console.log("createObj", createObj)
+  const queryString =
+  'INSERT INTO created_posts (user_id, title, description, url, photo_url) VALUES ($1, $2, $3, $4, $5);'
+  return db.query(queryString, [1, createObj.title, createObj.description, createObj.urlLink, createObj.photoUrl])
   .then((data) => {
     console.log(data.rows);
     return data.rows;
   })
   .catch((err) => {
     console.log(err.message);
+    return err;
   });
 }
 
+module.exports = { createPost };
 
+const addProperty = function(property) {
+  return pool.query(
+    `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms,
+    country,
+    street,
+    city,
+    province,
+    post_code,
+    active)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code, property.active])
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { createPost } = require('../db/queries/createPost');
 const router = express.Router();
+
 
 router.get('/', (req, res) => {
   res.redirect('/');
@@ -9,12 +11,30 @@ router.get('/create', (req, res) => {
   res.render('create');
 });
 
-router.post('/create', (req, res) => {
-  res.render('');
+router.post('/create', async(req, res) => {
+  try {
+    const created = await createPost(req.body);
+    res.json({created})
+  } catch (error) {
+    res.send('Something went wrong');
+  }
+  // res.render('');
 });
 
 router.get('/:id', (req, res) => {
   res.send(`individual post with id`);
+});
+
+router.post('/properties', (req, res) => {
+  const userId = req.session.userId;
+  database.addProperty({...req.body, owner_id: userId})
+    .then(property => {
+      res.send(property);
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e)
+    });
 });
 
 module.exports = router;
