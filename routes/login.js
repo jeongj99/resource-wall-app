4dd9fd3970b08da7e49c74e0183eadc771d8a7f1
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-24 15:44:29 +0000

    Added new styling to navigation bar and body of webpage in main.scss file. Created new login views page to render the same styles on the login page. This style will also be applied to register page in the future

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('login page')
+  res.render('login')
 });
 
 module.exports = router;
