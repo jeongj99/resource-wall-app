ad6b1305aac1205e9285c92f990fa0b43170f2f2
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-25 02:52:18 +0000

    Set up /register post route and add input field requirements for login and register

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('register')
+  res.render('register');
+});
+
+router.post('/', (req, res) => {
+  const { firstName, lastName, email, password, handler } = req.body;
+
+
 });
 
 module.exports = router;
