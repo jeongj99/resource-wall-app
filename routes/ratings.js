0e74e85d6abf59156480eccb0c37414a3eacf503
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-26 14:49:39 +0000

    Created API route and logic for creating rating

--- routes/ratings.js
+++ routes/ratings.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const router  = express.Router();
-const commentsQueries = require('../db/queries/ratingPost')
+const ratingsQueries = require('../db/queries/ratingPost')
+
+//Ratings - REST API CRUD
+//Create - POST
+
+router.post('/', (req, res) => {
+  const newRating = {
+    rating: req.body.rating,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  }
+  ratingsQueries.createRating(newRating)
+  .then((rating) => {
+    res.redirect(`/post/${req.body.post_id}`);
+  })
+});
