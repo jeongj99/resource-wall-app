4331fa333630a9e0e08d3efb822bfa8e942c7a87
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-26 14:33:54 +0000

    Created rating query file for future rating implementation

---
+++ db/queries/rating.js
@@ -0,0 +1,11 @@
+const db = require('../connection');
+
+//This function will take in the input the user put in the rating form and deposit it in the database.
+
+const createRating = (body) => {
+  const queryString =
+  'Insert into'
+
+
+
+}
