e4d4531323fb5e231f7a88fe3ac8dfca50d2569b
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-26 19:09:28 +0000

    Added further crud route skeletons for future work

--- routes/savePostRoutes.js
+++ routes/savePostRoutes.js
@@ -18,5 +18,33 @@ router.post('/', (req, res) => {
   })
 });
 
+//Note: the remainder of CRUD operations were added and kept as they will be worked on in the future.
+
+//Read All - GET
+router.get('/', (req, res) => {
+  res.send({ message: 'hello from Read all'});
+});
+
+//Read one - GET
+router.get('/:id', (req, res) => {
+  console.log(req.params);
+  res.send({ message: 'hello from Read one'});
+});
+
+//Update - POST
+router.post('/:id', (req, res) => {
+  console.log(req.params);
+  res.send({ message: 'hello from Update Post'});
+});
+
+//Delete - POST
+router.post('/:id/delete', (req, res) => {
+  console.log(req.params);
+  res.send({ message: 'hello from Delete Post'});
+});
+
+module.exports = router;
+
+
 module.exports = router;
 
