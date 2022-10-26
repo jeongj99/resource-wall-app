11ddbc38d033ad6b6b4b049e44d4c1f27d7c10be
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-26 15:57:30 +0000

    Fix bug in search route

--- routes/myProfile.js
+++ routes/myProfile.js
@@ -1,12 +1,29 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const homeHelpers = require('../db/queries/home');
 
 router.get('/', (req, res) => {
-  res.send('this is my profile page!');
+  const userLoggedIn = req.session.user_id;
+  if (!userLoggedIn) {
+    res.redirect('/login');
+  } else {
+    homeHelpers.getUserById(userLoggedIn).then(user => {
+      console.log(user);
+      res.render('myProfile', { userLoggedIn, user });
+    });
+  }
 });
 
-router.get('/edit-profile', (req, res) => {
-  res.send('edit profile!');
+router.get('/edit', (req, res) => {
+  const userLoggedIn = req.session.user_id;
+  if (!userLoggedIn) {
+    res.redirect('/login');
+  } else {
+    homeHelpers.getUserById(userLoggedIn).then(user => {
+      console.log(user);
+      res.render('editProfile', { userLoggedIn, user });
+    });
+  }
 });
 
 module.exports = router;
