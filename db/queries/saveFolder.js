2bfcaa7c9955a12ec30029fc745e856762fa00e2
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-26 19:56:02 +0000

    Created a new query file for saving a folder by its title into the database, along with it's accompanying crud routes file

---
+++ db/queries/saveFolder.js
@@ -0,0 +1,20 @@
+const db = require('../connection');
+
+//This function will allow a user to save a folder in the database, tied to their own user_id
+//This function will be called within the saveFolders route within the routes folder.
+
+const createSaveFolder = (body) => {
+  const queryString =
+  `INSERT INTO folders(user_id, title) VALUES ($1, $2) RETURNING *`
+  return db.query(queryString, [body.user_id, body.title])
+  .then((data) => {
+    console.log(data.rows[0]);
+    return data.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+    return err;
+  });
+}
+
+module.exports = { createSaveFolder }
---
+++ routes/saveFolderRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router  = express.Router();
+
+const saveFolderQueries = require('../db/queries/saveFolder')
+
